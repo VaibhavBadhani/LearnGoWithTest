dba890df3470fed1de0c40ed031c004854e521e3
Vaibhav Badhani <vaibhav.badhani-ext@infra.market>
2025-08-26 12:03:36 +0530

    Module 1 completed

--- hello_test.go
+++ hello_test.go
@@ -2,11 +2,40 @@ package main
 
 import "testing"
 
-func TestHello(t *testing.T){
-	got := Hello()
-	want := "Hello Worlds"
+// func TestHello(t *testing.T) {
+// 	got := Hello("vaibhava")
+// 	want := "Hello, vaibhava"
 
-	if got != want{
+// 	if got != want {
+// 		t.Errorf("got %q want %q", got, want)
+// 	}
+// }
+
+func TestHello(t *testing.T) {
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris", "english")
+		want := "Hello, Chris"
+
+		assertCorrectResults(t, got, want)
+
+	})
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("", french)
+		want := "Bonjour, World"
+
+		assertCorrectResults(t, got, want)
+	})
+	t.Run("greeting in Spanish", func(t *testing.T) {
+		got := Hello("Neymar", "Spanish")
+		want := "Hola amigo, Neymar"
+
+		assertCorrectResults(t, got, want)
+	})
+}
+
+func assertCorrectResults(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
 }
--- main.go
+++ main.go
@@ -2,10 +2,37 @@ package main
 
 import "fmt"
 
-func main(){
-	fmt.Println(Hello())
+// $ go doc fmt   [Learn Go ddocumention in terminal]
+
+func main() {
+	fmt.Println(Hello("vaibhava", "english"))
 }
 
-func Hello() string {
-	return "Hello World"
+const (
+	spanish = "Spanish"
+	french  = "French"
+
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola amigo, "
+	frenchHelloPrefix  = "Bonjour, "
+)
+
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
 }
