c1de3ae418360e126392207fb5b8b84c360c38ef
Vaibhav Badhani <vaibhav.badhani-ext@infra.market>
2025-09-30 00:50:37 +0530

    reflection+context

---
+++ reflection/reflection.go
@@ -0,0 +1,33 @@
+package reflection
+
+import "reflect"
+
+// Reflection in computing is the ability of a program to examine its own structure,
+// particularly through types; it's a form of metaprogramming. It's also a great source of confusion.
+
+func walk(x interface{}, fn func(input string)) {
+	val := getValue(x)
+
+	switch val.Kind() {
+	case reflect.Struct:
+		for i := 0; i < val.NumField(); i++ {
+			walk(val.Field(i).Interface(), fn)
+		}
+	case reflect.Slice:
+		for i := 0; i < val.Len(); i++ {
+			walk(val.Index(i).Interface(), fn)
+		}
+	case reflect.String:
+		fn(val.String())
+	}
+}
+
+func getValue(x interface{}) reflect.Value {
+	val := reflect.ValueOf(x)
+
+	if val.Kind() == reflect.Pointer {
+		val = val.Elem()
+	}
+
+	return val
+}
